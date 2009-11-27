5d06944dde7de80b0ed295058b5cff7f4546380a
marko.struna <marko.struna@49de09a6-d112-11de-8184-f791cb96e2ed>
2009-11-27 13:30:28 +0000

    Only backup in case something goes wrong.

---
+++ tags/091127_Backup/MSDefrag/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MSDefrag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+        	bmp.Dispose();
+        	fontBrush.Dispose();
+        	m_font.Dispose();
+        	statusBmp.Dispose();
+        	backBrush.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.statusPanel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusPanel.SuspendLayout();
+            this.panelDraw.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.366864F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.45562F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.75148F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3.254438F));
+            this.tableLayoutPanel1.Controls.Add(this.statusPanel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelDraw, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.89923F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.131783F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.263566F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.05426F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.457365F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1292, 866);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // statusPanel
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.statusPanel, 8);
+            this.statusPanel.Controls.Add(this.pictureBox2);
+            this.statusPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusPanel.Location = new System.Drawing.Point(33, 3);
+            this.statusPanel.Name = "statusPanel";
+            this.statusPanel.Size = new System.Drawing.Size(1212, 184);
+            this.statusPanel.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1212, 184);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panelDraw
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panelDraw, 8);
+            this.panelDraw.Controls.Add(this.pictureBox1);
+            this.panelDraw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDraw.Location = new System.Drawing.Point(33, 211);
+            this.panelDraw.Name = "panelDraw";
+            this.tableLayoutPanel1.SetRowSpan(this.panelDraw, 7);
+            this.panelDraw.Size = new System.Drawing.Size(1212, 607);
+            this.panelDraw.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1212, 607);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 866);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Marko\'s Defragmentation Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnGuiClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.statusPanel.ResumeLayout(false);
+            this.panelDraw.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel statusPanel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ tags/091127_Backup/MSDefrag/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MSDefragLib;
+using System.Threading;
+using System.Timers;
+using MSDefragLib.FileSystem.Ntfs;
+
+namespace MSDefrag
+{
+    public partial class Form1 : Form
+    {
+        public void Defrag()
+        {
+            m_msDefragLib.RunJkDefrag("C:\\*", 2, 100, 10, null, null);
+        }
+
+        // This will be called whenever the list changes.
+        private void ShowChanges(object sender, EventArgs e)
+        {
+            String message = "";
+            UInt32 level = 0;
+
+            if (e is MSScanNtfsEventArgs)
+            {
+                MSScanNtfsEventArgs ev = (MSScanNtfsEventArgs)e;
+                message = ev.m_message;
+                level = ev.m_level;
+            }
+
+            BeginInvoke(new MethodInvoker(delegate { AddStatusMessage(level, message); }));
+        }
+
+        private void AddStatusMessage(UInt32 level, String message)
+        {
+            messages[level] = message;
+
+            PaintStatus();
+        }
+
+        //TODO: use standard controls, do not paint ourselves (unperformant)
+        private void PaintStatus()
+        {
+            Graphics g1 = Graphics.FromImage(statusBmp);
+
+            g1.FillRectangle(backBrush, m_rec2);
+
+            for (int ii = 0; ii < maxMessages; ii++)
+            {
+                if (messages[ii] == null) continue;
+
+                String mess = messages[ii];
+
+                g1.DrawString(mess, m_font, fontBrush, new PointF(0, 15 * ii));
+            }
+            pictureBox2.Refresh();
+        }
+
+        private void DrawCluster(object sender, EventArgs e)
+        {
+            if (e is DrawClusterEventArgs2)
+            {
+                DrawClusterEventArgs2 dcea = (DrawClusterEventArgs2)e;
+
+                DrawSquares();
+            }
+        }
+
+        private void Notified(object sender, EventArgs e)
+        {
+            if (e is NotifyGuiEventArgs)
+            {
+                NotifyGuiEventArgs ngea = (NotifyGuiEventArgs)e;
+
+//                DrawDirtySquare(ngea.m_clusterSquare);
+
+                BeginInvoke(new MethodInvoker(delegate { DrawSquares(); }));
+            }
+        }
+
+        public Form1()
+        {
+            m_msDefragLib = new MSDefragLib.MSDefragLib();
+
+            m_msDefragLib.ShowDebugEvent += new MSDefragLib.MSDefragLib.ShowDebugHandler(ShowChanges);
+            m_msDefragLib.DrawClusterEvent += new MSDefragLib.MSDefragLib.DrawClusterHandler(DrawCluster);
+            m_msDefragLib.NotifyGuiEvent += new MSDefragLib.MSDefragLib.NotifyGuiHandler(Notified);
+
+            InitializeComponent();
+
+            InitializeDiskMap();
+
+
+            //Defrag();
+
+            defragThread = new Thread(Defrag);
+
+            defragThread.Start();
+
+            //aTimer = new System.Timers.Timer(500);
+
+            //aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            //aTimer.Interval = 1000;
+            //aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            //m_form.DrawSquares(0, m_form.m_numSquares);
+            //m_form.PaintStatus();
+        }
+
+        public void InitializeDiskMap()
+        {
+            m_numSquaresX = pictureBox1.Width / m_squareSize;
+            m_numSquaresY = pictureBox1.Height / m_squareSize;
+
+            m_numSquares = m_numSquaresX * m_numSquaresY;
+
+            m_msDefragLib.NumSquares = m_numSquares;
+
+            pictureBox1.Image = bmp;
+            pictureBox2.Image = statusBmp;
+            DrawSquares();
+        }
+
+        private void DrawSquares()
+        {
+            IList<MSDefragLib.ClusterSquare> squaresList = m_msDefragLib.DirtySquares;
+
+            if (squaresList == null)
+                return;
+
+            Graphics g1 = Graphics.FromImage(bmp);
+
+            foreach (MSDefragLib.ClusterSquare square in squaresList)
+            {
+                Int32 squareIndex = square.m_squareIndex;
+                MSDefragLib.MSDefragLib.CLUSTER_COLORS color = square.m_color;
+
+                Int32 posX = (Int32)(squareIndex % m_numSquaresX);
+                Int32 posY = (Int32)(squareIndex / m_numSquaresX);
+
+                Rectangle rec = new Rectangle(posX * m_squareSize, posY * m_squareSize, m_squareSize - 1, m_squareSize - 1);
+
+                Color col = Color.White;
+
+                switch (color)
+                {
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORUNMOVABLE:
+                        col = Color.Red;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORALLOCATED:
+                        col = Color.Yellow;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORBACK:
+                        col = Color.White;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORBUSY:
+                        col = Color.Blue;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLOREMPTY:
+                        col = Color.White;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORFRAGMENTED:
+                        col = Color.Orange;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORMFT:
+                        col = Color.Pink;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORSPACEHOG:
+                        col = Color.GreenYellow;
+                        break;
+                    case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORUNFRAGMENTED:
+                        col = Color.Green;
+                        break;
+                    default:
+                        col = Color.White;
+                        break;
+                }
+                SolidBrush brush = new SolidBrush(col);
+                g1.FillRectangle(brush, rec);
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void DrawDirtySquare(ClusterSquare square)
+        {
+            if (square == null)
+            {
+                return;
+            }
+            Graphics g1 = Graphics.FromImage(bmp);
+
+            Int32 squareIndex = square.m_squareIndex;
+            MSDefragLib.MSDefragLib.CLUSTER_COLORS color = square.m_color;
+
+            Int32 posX = (Int32)(squareIndex % m_numSquaresX);
+            Int32 posY = (Int32)(squareIndex / m_numSquaresX);
+
+//                AddStatusMessage(1, "Square: " + squareIndex);
+
+            Rectangle rec = new Rectangle(posX * m_squareSize, posY * m_squareSize, m_squareSize - 1, m_squareSize - 1);
+
+            Color col = Color.White;
+
+            switch (color)
+            {
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORUNMOVABLE:
+                    col = Color.Red;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORALLOCATED:
+                    col = Color.Yellow;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORBACK:
+                    col = Color.White;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORBUSY:
+                    col = Color.Blue;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLOREMPTY:
+                    col = Color.White;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORFRAGMENTED:
+                    col = Color.Orange;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORMFT:
+                    col = Color.Pink;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORSPACEHOG:
+                    col = Color.GreenYellow;
+                    break;
+                case MSDefragLib.MSDefragLib.CLUSTER_COLORS.COLORUNFRAGMENTED:
+                    col = Color.Green;
+                    break;
+                default:
+                    col = Color.White;
+                    break;
+            }
+
+            SolidBrush brush = new SolidBrush(col);
+
+            g1.FillRectangle(brush, rec);
+
+            pictureBox1.Refresh();
+        }
+
+        Thread defragThread = null;
+
+        MSDefragLib.MSDefragLib m_msDefragLib = null;
+
+        private const int maxMessages = 7;
+
+        String[] messages = new String[maxMessages];
+
+        Bitmap bmp = new Bitmap(1000, 900);
+        Bitmap statusBmp = new Bitmap(1000, 800);
+
+        Rectangle m_rec2 = new Rectangle(0, 0, 1000, 800);
+
+        Font m_font = new Font("Tahoma", 10);
+
+        SolidBrush backBrush = new SolidBrush(Color.Blue);
+        SolidBrush fontBrush = new SolidBrush(Color.Yellow);
+
+        public const Int32 maxSquare = 5000;
+
+        private const Int32 m_squareSize = 7;
+
+        Int32 m_numSquaresX = 1;
+        Int32 m_numSquaresY = 1;
+
+        private Int32 m_numSquares = 1;
+
+        private void OnGuiClosing(object sender, FormClosingEventArgs e)
+        {
+            m_msDefragLib.StopJkDefrag(1000);
+
+            if (defragThread.IsAlive)
+            {
+                try
+                {
+                    defragThread.Abort();
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                while (defragThread.IsAlive)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+    }
+}
